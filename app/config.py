1e1af7acf56e08f4eea2af880609b6326de19327
Luis Felipe Serna Mendoza <lfserna@umsa.bo>
2026-06-11 19:52:40 -0400

    Agregar configuracion separada

---
+++ app/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "linki-health-demo-secret")
+
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_PORT = int(os.getenv("DB_PORT", "3306"))
+    DB_USER = os.getenv("DB_USER", "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "123456")
+    DB_NAME = os.getenv("DB_NAME", "linki_health_citas")
